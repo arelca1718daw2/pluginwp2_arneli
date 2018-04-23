6243c967beb953b437bf50836a1513e02822b22f
root <root@debian9v1.fjeclot.net>
2018-04-23 18:53:18 +0200

    prova1 peupagina

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Arnau Elias
+Author URI: https://github.com/arelca1718daw2/pluginwp2_arneli.git
+License: GNU GPLv3 
+*/
+?>
